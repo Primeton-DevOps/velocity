562c25114446b4cff8beadbac1111334758ae956
ZhongWen Li <lizhongwen1989@gmail.com>
2016-11-16 21:06:36 +0800

    Update

---
+++ src/test/java/com/primeton/devops/velocity/AbstractTestCase.java
@@ -0,0 +1,82 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2001-2016 Primeton Technologies, Ltd.
+ * All rights reserved.
+ * 
+ * Created on Nov 16, 2016 8:21:53 PM
+ *******************************************************************************/
+
+package com.primeton.devops.velocity;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * AbstractTestCase.
+ *
+ * @author ZhongWen Li (mailto:lizw@primeton.com)
+ */
+public abstract class AbstractTestCase {
+	
+	protected static final String PATH_PREFIX = "classpath:"; //$NON-NLS-1$
+
+	@Before
+	public void init() {
+	}
+	
+	@Test
+	public abstract void test() throws Exception;
+
+	@After
+	public void clean() {
+	}
+	
+	/**
+	 * 
+	 * @param filePath
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	protected InputStream getTemplateAsStream(String filePath) throws FileNotFoundException {
+		if (StringUtils.isBlank(filePath)) {
+			return null;
+		}
+		if (filePath.startsWith(PATH_PREFIX) && filePath.length() > PATH_PREFIX.length()) {
+			filePath = filePath.substring(PATH_PREFIX.length());
+			return AbstractTestCase.class.getResourceAsStream(filePath);
+		} else {
+			return new FileInputStream(filePath);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param filePath
+	 * @return
+	 * @throws FileNotFoundException
+	 */
+	protected String getTemplateAsString(String filePath) throws FileNotFoundException {
+		InputStream in = getTemplateAsStream(filePath);
+		if (null == in) {
+			return null;
+		}
+		try {
+			return IOUtils.toString(in, Charset.forName("utf-8")); //$NON-NLS-1$
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			IOUtils.closeQuietly(in);
+		}
+		return null;
+	}
+
+}
---
+++ src/test/java/com/primeton/devops/velocity/HelloTestCase.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2001-2016 Primeton Technologies, Ltd.
+ * All rights reserved.
+ * 
+ * Created on Nov 16, 2016 8:20:28 PM
+ *******************************************************************************/
+
+package com.primeton.devops.velocity;
+
+/**
+ * HelloTestCase.
+ *
+ * @author ZhongWen Li (mailto:lizw@primeton.com)
+ */
+public class HelloTestCase extends AbstractTestCase {
+
+	/* (non-Javadoc)
+	 * @see com.primeton.devops.velocity.AbstractTestCase#test()
+	 */
+	@Override
+	public void test() throws Exception {
+		// Auto-generated method stub
+		
+	}
+	
+
+}
